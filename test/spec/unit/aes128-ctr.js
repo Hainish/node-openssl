756ffcab8915f7950686c031393e62f4c8ffe8fb
William Budington <bill.budington@gmail.com>
2012-07-23 20:29:09 -0700

    adding decryption test as well

--- test/spec/unit/aes128-ctr.js
+++ test/spec/unit/aes128-ctr.js
@@ -3,17 +3,21 @@ var assert = require('assert');
 var cipher;
 
 describe('aes128-ctr', function(){
+  var actual_ciphertext_buffer;
+  var key = new Buffer('thiskeyisverybad', 'ascii');
+  var pt = new Buffer("thisblockverybadthisclockbery", 'ascii');
   before(function(){
     cipher = openssl();
   });
 
   it('should encrypt a buffer successfully', function(){
-    var actual_buffer = cipher.aes128_ctr_encrypt(new Buffer('thiskeyisverybad', 'ascii'), new Buffer("thisblockverybadthisclockbery", 'ascii'));
-    var expected_buffer = new Buffer('c5648533a4ac43d457d169615f30b98a79ea83b51fd59e11813b484ebf', 'hex');
-    assert.deepEqual(actual_buffer, expected_buffer, "The buffer ciphertext did not match the expected ciphertext");
+    actual_ciphertext_buffer = cipher.aes128_ctr_encrypt(key, pt);
+    var expected_ciphertext_buffer = new Buffer('c5648533a4ac43d457d169615f30b98a79ea83b51fd59e11813b484ebf', 'hex');
+    assert.deepEqual(actual_ciphertext_buffer, expected_ciphertext_buffer, "The buffer ciphertext did not match the expected ciphertext");
   });
 
   it('should decrypt a buffer successfully', function(){
-    
+    var actual_plaintext_buffer = cipher.aes128_ctr_decrypt(key, actual_ciphertext_buffer);
+    assert.deepEqual(pt, actual_plaintext_buffer, "The decrypted buffer did not match the original plaintext");
   });
 });
